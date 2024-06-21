e2fb080b72ac53f0400a59bb7993ee5b89553fad
caiies <jxii3d@outlook.com>
2024-06-21 11:37:32 +0800

    Modify very.cpp

--- very.cpp
+++ very.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tree high I guess.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
