bd207256a376930166a2617f49bc440061c2949b
caiies <jxii3d@outlook.com>
2024-07-09 01:01:24 +0800

    Modify box.c

--- box.c
+++ box.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Before attorney speech executive on staff.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
