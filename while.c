3d9d7a52d5326d34b22e1d17b89e4f88ba073080
caiies <jxii3d@outlook.com>
2024-07-23 15:42:09 +0800

    Modify while.c

--- while.c
+++ while.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Away blue manager hundred cost someone perhaps.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
