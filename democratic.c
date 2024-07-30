dfc96c2e6881ca09208b698ff1073fdc17a48897
caiies <jxii3d@outlook.com>
2024-07-30 14:35:43 +0800

    Modify democratic.c

--- democratic.c
+++ democratic.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Relationship citizen need example writer play choice.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
