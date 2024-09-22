72987f49b82bf0a0de2bfecffd8923977a9d647c
caiies <jxii3d@outlook.com>
2024-09-22 11:16:05 +0800

    Modify seven.c

--- seven.c
+++ seven.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Official region partner budget game beautiful.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
