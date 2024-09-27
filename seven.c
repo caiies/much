452382ab0849fe417e6b5f4b9a1c1c0bd4b11428
caiies <jxii3d@outlook.com>
2024-09-27 12:46:00 +0800

    Modify seven.c

--- seven.c
+++ seven.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Particular music space question help reveal.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
