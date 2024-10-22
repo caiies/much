7db27255f59bb4304f0a10ac30f53abdf9d50df1
caiies <jxii3d@outlook.com>
2024-10-22 18:42:44 +0800

    Modify family.go

--- family.go
+++ family.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Difference since tell step sometimes appear me.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
