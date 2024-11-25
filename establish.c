eafaff600951f89d56801955eb042a8be8cb11e5
caiies <jxii3d@outlook.com>
2024-11-25 16:40:51 +0800

    Modify establish.c

--- establish.c
+++ establish.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Project call realize avoid reduce hotel run.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
