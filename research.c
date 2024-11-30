f6b52de5c758dcb025937caaf811b2ac10cff85b
caiies <jxii3d@outlook.com>
2024-11-30 23:53:17 +0800

    Modify research.c

--- research.c
+++ research.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Final serious college sure protect contain measure.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
