2b78e6809b2cc1ddf598c3c3b7f2d7cb33086ff0
caiies <162767504+caiies@users.noreply.github.com>
2025-03-26 21:27:03 +0800

    refactor(core): restructure have.c code

--- have.c
+++ have.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-int main() {
-	printf("Hello, World!\n");
-	return 0;
-}
 
-# Though site civil enough well.
-#include <stdio.h>
 int main() {
-	printf("Hello, World!\n");
-	return 0;
+    printf("Hello, World!\n");
+    return 0;
 }
